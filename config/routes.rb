82f38489c3cd59bd5e8246dc30024f6f3f105720
yuuuba <nsabu45@gmail.com>
2023-06-08 15:15:31 +0900

    mypageまで

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'communities#new'
-  devise_for :users
+  get "users/:id" , to: "users#show", as: 'user'
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  get "mypage", to: "users#mypage"
   resources :communities
 end
